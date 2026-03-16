3e5ccae44bfd6584e85143b698b5467f71e89eb3
nvdchw <navidchew@gmail.com>
2026-03-16 23:15:48 +0800

    Update Note validation regex comment

--- src/main/java/seedu/address/model/person/Note.java
+++ src/main/java/seedu/address/model/person/Note.java
@@ -10,7 +10,8 @@ public class Note {
     public static final String MESSAGE_CONSTRAINTS = "Notes can be any values, and it should not be null";
 
     /*
-     * Allows any character, also empty strings.
+     * Allows up to 500 characters consisting of letters, numbers, punctuation,
+     * symbols, spaces, tabs, and line breaks.
      */
     public static final String VALIDATION_REGEX = "^[\\p{L}\\p{N}\\p{P}\\p{S}\\p{Zs}\\r\\n\\t]{0,500}$";
     public final String value;
