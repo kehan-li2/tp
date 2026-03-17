d4b5237209f97c0654a6c185d03791e51639b3ad
Clara Ng <x3Clarawork@gmail.com>
2026-03-17 18:08:12 +0800

    Add edge case test for sorting date time

--- src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -47,6 +47,20 @@ public class ListCommandParserTest {
                 " " + PREFIX_SORT + "visit",
                 new ListCommand("visit"));
     }
+
+    @Test
+    public void parse_caseInsensitiveSortVisit_success() {
+        assertParseSuccess(parser,
+                " " + PREFIX_SORT + "Visit",
+                new ListCommand("visit"));
+    }
+
+    @Test
+    public void parse_invalidSortField_failure() {
+        assertParseFailure(parser,
+                " " + PREFIX_SORT + "date",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+    }
 }
 
 
